d65eb0ed475cdadd6838e0cca49b75a222c1852a
Nugget-Meister <tremainecamacho@gmail.com>
2023-05-24 21:45:39 -0400

    initialize file and functions, completed 1?

---
+++ index.js
@@ -0,0 +1,22 @@
+const words = require("./words.json")
+
+/** 
+ * Logs all of the words in the given array
+ * @param {Array} list - An array of all the words
+ * @returns {null}
+*/
+function allWords (list) {
+    for (word of list){
+        console.log(word)
+    }
+}
+function firstTenWords (list){}
+function firstXWords(list){}
+function subsetOfWords(list){}
+function sortWords(list){}
+function wordsWithQ(list){}
+function findWordsWithLetter(list, letter){}
+
+// allWords(words)
+
+
