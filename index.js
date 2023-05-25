de6759ca05e46e587be5386aface65358ae9ec74
Nugget-Meister <tremainecamacho@gmail.com>
2023-05-24 21:52:05 -0400

    complete third function, added missing function, jsdoc

--- index.js
+++ index.js
@@ -10,13 +10,37 @@ function allWords (list) {
         console.log(word)
     }
 }
-function firstTenWords (list){}
+/**
+ * Logs the first ten words in the given array
+ * @param {Array} list - An array of all the words
+ * @returns {null}
+ */
+function firstTenWords (list) {
+    for (let i = 0; i < 10; i++) {
+        console.log(list[i])
+    }
+}
+
+/**
+ * Logs the next ten words in the given array
+ * @param {Array} list - An array of all the words
+ */
+function nextTenWords(list) {
+    for (let i = 10; i < 20; i++) {
+        console.log(list[i])
+    }
+}
+
 function firstXWords(list){}
 function subsetOfWords(list){}
 function sortWords(list){}
 function wordsWithQ(list){}
 function findWordsWithLetter(list, letter){}
 
-// allWords(words)
+// allWords(words) 
+// firstTenWords(words)
+// nextTenWords(words)
+
+
 
 
