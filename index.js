1637286a0e2363ec432144cb29237e159ddce174
Nicole Slater <nicoleslater@pursuit.org>
2023-05-26 19:54:18 -0400

    Placing functions

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
 const words = require("./words.json");
+
+/** Logs all the words */
+function allWords(){}
+
+/** Logs the first ten words */
+function firstTenWords(){}
+
+/** Logs next 10 words */
+function nextTenWords(){}
+
+
+/** Logs 0 to x words */
+function firstXWords(){}
+
+
+
+/** Logs from x to y words */
+function subsetOfWords(){}
+
+
+/** Logs all the words sorted Alphbetically */
+function sortWords(){}
+
+
+module.exports = {
+    allWords, 
+    firstTenWords, 
+    nextTenWords,
+    firstXWords, 
+    subsetOfWords,
+    softWords
+}
+
+
