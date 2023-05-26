e2400db7cc30bfed8c7fb3c7d32eacbfebc1e9ca
AishaCKleemoff <96323559+AishaCKleemoff@users.noreply.github.com>
2023-05-26 07:46:07 -0400

    add first commit

---
+++ index.js
@@ -0,0 +1,17 @@
+const wordsJSON = require("./words.json")
+console.log(wordsJSON);
+
+function allWords() {
+    
+};
+function firstTenWords() { };
+function nextTenWords() { };
+function firstXWords() { };
+function subsetOfWords(params) {
+    
+};
+
+function sortWords(params) {
+    
+};
+
