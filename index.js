a0610fe5b73c33ac625411302d4118cab7c2a0a6
AishaCKleemoff <96323559+AishaCKleemoff@users.noreply.github.com>
2023-05-27 09:58:19 -0400

    add another commit

--- index.js
+++ index.js
@@ -1,10 +1,20 @@
-const wordsJSON = require("./words.json")
-console.log(wordsJSON);
+const words = require("./words.json")
+//console.log(words);
 
-function allWords() {
-    
+/*function allWords() {
+    for (const word of words) {
+        console.log(word);
+    }
 };
-function firstTenWords() { };
+allWords();*/
+
+function firstTenWords(words) { 
+    for (let i = 0; i < 10; i++) {
+        console.log(words[i]);
+    }
+};
+firstTenWords(words);
+
 function nextTenWords() { };
 function firstXWords() { };
 function subsetOfWords(params) {
