cb5419b40dccb8168cef345a9f65a28fc46e37fd
Shanice Griffin <shanicegriffin@Shanices-MacBook-Air.local>
2023-05-27 16:20:43 -0400

    working on user input

--- index.js
+++ index.js
@@ -1,3 +1,63 @@
-const wordsJSON = require("./words.json")
+const words = require("./words.json")
 
-console.log(wordsJSON)
+function allWords(words) {
+    return words;
+}
+
+function firstTenWords(words) {
+    return words.slice(0, 10);
+}
+
+function nextTenWords(words) {
+    return words.slice(10, 20);
+}
+
+function firstXWords(words, x) {
+    return words.slice(0, x);
+}
+
+const subsetOfWords = (words, x, y) => {
+    return words.slice(x, y);
+}
+
+const sortWords = (words) => {
+    return words.sort();
+} 
+// console.log(sortWords(words))
+
+
+
+//Begin wordle solver function
+
+function wordsWithQ(words) {
+    let qWords = [];
+
+    for (let word of words) {
+        let splitWord = word.split("");
+
+        for (let i = 0; i <= splitWord.length; i++) {
+            if(splitWord[i] == "q") {
+                qWords.push(word);
+            }
+        }
+    }
+    return qWords;
+}
+//console.log(wordsWithQ(words))
+
+
+function findWordsWithLetter(words, letter) {
+    let foundWordsByLetter = [];
+
+    for (let word of words) {
+        let splitWord = word.split("");
+
+        for (let i = 0; i <= splitWord.length; i++) {
+            if(splitWord[i] == letter) {
+                foundWordsByLetter.push(word);
+            }
+        }
+    }
+    return foundWordsByLetter;
+}
+console.log(findWordsWithLetter(words, "t"))
