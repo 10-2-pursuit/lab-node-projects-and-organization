7d77a2fa83e15ed1d02316b67c1bdffd933bb3ac
Nicole Slater <nicoleslater@pursuit.org>
2023-05-26 20:22:00 -0400

    Looking through

--- index.js
+++ index.js
@@ -1,26 +1,28 @@
 const words = require("./words.json");
 
 /** Logs all the words */
-function allWords(){}
-
+function allWords(words){}
+return words;
 /** Logs the first ten words */
-function firstTenWords(){}
+function firstTenWords(words){}
 
 /** Logs next 10 words */
-function nextTenWords(){}
+function nextTenWords(words){}
 
 
 /** Logs 0 to x words */
-function firstXWords(){}
+function firstXWords(words){}
 
 
 
 /** Logs from x to y words */
-function subsetOfWords(){}
+function subsetOfWords(words){}
 
 
 /** Logs all the words sorted Alphbetically */
-function sortWords(){}
+function sortWords(words){
+    return words.sort();
+}
 
 
 module.exports = {
