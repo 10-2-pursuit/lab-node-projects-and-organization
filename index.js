82469206e18d1f05bb533002202263d4f8ba850b
AishaCKleemoff <96323559+AishaCKleemoff@users.noreply.github.com>
2023-05-28 13:43:07 -0400

    add last function.

--- index.js
+++ index.js
@@ -37,4 +37,36 @@ function subsetOfWords(words, xSubSet, ySubSet) {
 subsetOfWords(words, Number(process.argv[2]), Number(process.argv[3]));
 console.log(process.argv);
 
-function sortWords(params) {}
+function sortWords(words) {
+    const wordsSorted = words.sort((a, b) => {
+        if (a.toLowerCase() < b.toLowerCase()) {
+            return -1;
+        }
+        if (a.toLowerCase() > b.toLowerCase()) {
+            return 1;
+        }
+        return 0;
+    });
+    console.log(wordsSorted);
+}
+
+
+function wordsWithQ(words) {
+    const qWords = words.filter((word) => word.includes('q'));
+    return qWords;
+}
+
+
+function findWordsWithLetter(words, letter, position) {
+    const letterWords = words.filter((word) => {
+        if (position && word[position] === letter.toLowerCase()) {
+            return word;
+        }
+        if (!position && word.includes(letter.toLowerCase())) {
+            return word;
+        }
+    });
+    return letterWords;
+}
+ 
+console.log(findWordsWithLetter(words, process.argv[2], process.argv[3]));
