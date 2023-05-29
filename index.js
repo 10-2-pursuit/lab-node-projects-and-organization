1643e9791afeb7f7d7268bfbbf3d8babe8bb0b67
Nicole Slater <nicoleslater@pursuit.org>
2023-05-29 19:53:16 -0400

    Working through

--- index.js
+++ index.js
@@ -1,37 +1,44 @@
 const words = require("./words.json");
 
-/** Logs all the words */
-function allWords(words){}
-return words;
-/** Logs the first ten words */
-function firstTenWords(words){}
-
-/** Logs next 10 words */
-function nextTenWords(words){}
-
-
-/** Logs 0 to x words */
-function firstXWords(words){}
-
-
-
-/** Logs from x to y words */
-function subsetOfWords(words){}
-
-
-/** Logs all the words sorted Alphbetically */
-function sortWords(words){
-    return words.sort();
+function _allWords(words) {
+    return console.log(words)
 }
 
-
-module.exports = {
-    allWords, 
-    firstTenWords, 
-    nextTenWords,
-    firstXWords, 
-    subsetOfWords,
-    softWords
+function _firstTenWords(words) {
+    for(i =0; i < 10; i++) {
+        console.log(words[i])
+    }
 }
+
+function _nextTenWords(words) {
+    for(i = 10; i < 20; i++) {
+        console.log(words[i])
+    }
+}
+
+function _firstXWords(words,x) {
+    for (i = 0; i < x; i++) {
+        console.log(words[i])
+    }
+}
+
+function _subsetOfWords(words) {
+    for (i = x; i < y; i++) {
+        console.log(words[i])
+    }
+}
+
+function _sortWords(words) {
+    console.log(words.sort());
+}
+
+function wordsWithQ(words) {
+    return words.filter(word => word.includes("q"));
+}
+
+function findWordsWithLetter(words, letters) {
+    return words.filter(word => word.includes(process.argv[2]))
+}
+console.log(findWordsWithLetter(words, process.argv[2]));
 
 
